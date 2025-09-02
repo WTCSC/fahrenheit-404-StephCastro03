326ad5a0d362adc202349aadee7341b08b6fb694
Stephanie Castro <stephsnuggs1@gmail.com>
2025-09-02 14:45:15 -0600

    Started assignment

---
+++ Fahrenheit-404.py
@@ -0,0 +1,12 @@
+def c_to_f(c):
+    return (c * 1.8)+32
+def f_to_c(f):
+    return (f-32)*0.5555
+
+print ("Welcome to Farenheit-404 ")
+
+while True:
+    print("1. Convert Celcuis to Fahrenheit")
+    print("2. Convert Fahrenheit to Celcuis")
+
+    choice = input()
